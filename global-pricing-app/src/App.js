072820661e0d30960e01d3e6925e6953271d3f47
youssef <youssefmaghzaz@gmail.com>
2020-04-07 01:26:22 +0200

    add global app

---
+++ global-pricing-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import FrostApp from "frost"
+import Drought from "drought"
+
+const App = () => {
+  return (
+    <div>
+      <h1> hello App gglob </h1>
+      <FrostApp />
+      <Drought />
+    </div>
+  )
+}
+
+export default App
---
+++ global-pricing-app/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 5000
+  },
+  output: {
+    filename: "[hash].[name].js",
+    chunkFilename: "[hash].[name].js"
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./Public/index.html",
+      filename: "index.html"
+    })
+  ]
+};
